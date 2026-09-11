5f319778e225432366bba9f42df130773ed4f59f
TejUzumaki <tejasgafat785@gmail.com>
2026-09-11 12:05:53 +0530

    Fix: Add missing R import, bump Gradle plugin to 8.2.0

--- app/src/main/java/com/blueboss/terbility/ui/SettingsActivity.kt
+++ app/src/main/java/com/blueboss/terbility/ui/SettingsActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.blueboss.terbility.R
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
